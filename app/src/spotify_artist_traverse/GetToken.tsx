3d297ef34d100735a2d716e7a00da98d8275095d
dcep93 <dcep93@gmail.com>
2023-06-08 13:18:35 -0400

    x

--- app/src/spotify_artist_traverse/GetToken.tsx
+++ app/src/spotify_artist_traverse/GetToken.tsx
@@ -4,14 +4,19 @@ const CLIENT_ID = "613898add293422983bbba619d9cc8fa";
 const REDIRECT_URI = window.location.href.replace(/(\?|#).*/, "");
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
-const STORAGE_KEY = `access_token-${CLIENT_ID}`;
+const STORAGE_KEY = `v2-access_token-${CLIENT_ID}`;
 
 export function getStoredToken() {
-  return window.localStorage.getItem(STORAGE_KEY);
+  const stored = window.localStorage.getItem(STORAGE_KEY);
+  if (!stored) return {};
+  return JSON.parse(stored);
 }
 
+var partnerToken =
+  "BQASAQncNx-9wPJr2KnVsm6p8weuCq3ChRvBpeuFDidiYAHHQpU8t67WJsrzp1CKpJfVdToQ9VhhT9uxAlOeE87dgqxMN9MmfpHMA5H5GCstygK2u1WNIB2fy6FsxokNbpTXuVLf1cHmBrfcZcQEy8DZXDVcvnZ9wa2mj6mxv8mu6yE-hw8Qd5Tz6AO7PLxnrlAESpT2qTTpPm-EwN28PQRI1TPEHrTKRk312e6iUrTi6IhwuPwnPmT9EAfjCuWpmNlblrGhbzbnNUVofHRuqdLVzmDjy6jsr8HOgEdY54jyfcVbpZeJA4xllrexdm-Hvyma4w2E";
+
 export default function GetToken() {
-  const [token, setToken] = useState(getStoredToken());
+  const [token, setToken] = useState(getStoredToken().token);
 
   useEffect(() => {
     if (token) return;
@@ -25,7 +30,14 @@ export default function GetToken() {
         .find((elem) => elem.startsWith("access_token"))!
         .split("=")[1];
 
-      window.localStorage.setItem(STORAGE_KEY, storedToken);
+      window.localStorage.setItem(
+        STORAGE_KEY,
+        JSON.stringify({
+          token: storedToken,
+          partnerToken,
+          // partnerToken: prompt("enter your partner bearer token")!,
+        })
+      );
       setToken(storedToken);
     }
   }, [token]);
@@ -35,7 +47,7 @@ export default function GetToken() {
     loginUrl: `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`,
     logout: () => {
       window.localStorage.removeItem(STORAGE_KEY);
-      setToken("");
+      setToken(undefined);
     },
   };
 }
--- app/src/spotify_artist_traverse/fetcher.tsx
+++ app/src/spotify_artist_traverse/fetcher.tsx
@@ -19,12 +19,11 @@ export default function fetcher(
       "https://api-partner.spotify.com/pathfinder/v1/query?operationName=queryArtistOverview&variables=%7B%22uri%22%3A%"
     )
   );
-  // https://api-partner.spotify.com/pathfinder/v1/query?operationName=queryArtistOverview&variables=%7B%22uri%22%3A%22spotify%3Aartist%3A5JZ7CnR6gTvEMKX4g70Amv%22%2C%22locale%22%3A%22%22%2C%22includePrerelease%22%3Afalse%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2235648a112beb1794e39ab931365f6ae4a8d45e65396d641eeda94e4003d41497%22%7D%7D
   return Promise.resolve().then(() =>
     path.startsWith("/")
       ? fetch(`https://api.spotify.com/v1${path}`, {
           headers: {
-            Authorization: `Bearer ${getStoredToken()}`,
+            Authorization: `Bearer ${getStoredToken().token}`,
           },
         }).then((resp) => resp.json())
       : new Promise((resolve) =>
@@ -36,7 +35,7 @@ export default function fetcher(
                 url: path,
                 options: {
                   headers: {
-                    Authorization: `Bearer BQASAQncNx-9wPJr2KnVsm6p8weuCq3ChRvBpeuFDidiYAHHQpU8t67WJsrzp1CKpJfVdToQ9VhhT9uxAlOeE87dgqxMN9MmfpHMA5H5GCstygK2u1WNIB2fy6FsxokNbpTXuVLf1cHmBrfcZcQEy8DZXDVcvnZ9wa2mj6mxv8mu6yE-hw8Qd5Tz6AO7PLxnrlAESpT2qTTpPm-EwN28PQRI1TPEHrTKRk312e6iUrTi6IhwuPwnPmT9EAfjCuWpmNlblrGhbzbnNUVofHRuqdLVzmDjy6jsr8HOgEdY54jyfcVbpZeJA4xllrexdm-Hvyma4w2E`,
+                    Authorization: `Bearer ${getStoredToken().partnerToken}`,
                   },
                 },
               },
