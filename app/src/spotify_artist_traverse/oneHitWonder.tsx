833e7bae15621302903e799b22550cdb1eafb1d1
dcep93 <dcep93@gmail.com>
2023-06-09 00:21:29 -0400

    x

--- app/src/spotify_artist_traverse/oneHitWonder.tsx
+++ app/src/spotify_artist_traverse/oneHitWonder.tsx
@@ -11,12 +11,14 @@ export default function oneHitWonder(id: string) {
       '{"persistedQuery":{"version":1,"sha256Hash":"35648a112beb1794e39ab931365f6ae4a8d45e65396d641eeda94e4003d41497"}}',
   })
     .then((json) =>
-      json.data.artistUnion.discography.topTracks.items
-        .map((item: any) => ({
-          name: item.track.name,
-          playcount: parseInt(item.track.playcount),
-        }))
-        .sort((a: any, b: any) => b.playcount - a.playcount)
+      json === undefined
+        ? []
+        : json.data.artistUnion.discography.topTracks.items
+            .map((item: any) => ({
+              name: item.track.name,
+              playcount: parseInt(item.track.playcount),
+            }))
+            .sort((a: any, b: any) => b.playcount - a.playcount)
     )
     .then((tracks) =>
       tracks.length >= 2 &&
