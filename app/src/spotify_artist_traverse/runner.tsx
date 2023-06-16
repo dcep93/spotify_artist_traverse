0ca0382a34dd34e56596baee0ae21822a2ce37ab
dcep93 <dcep93@gmail.com>
2023-06-16 11:27:41 -0400

    x

--- app/src/spotify_artist_traverse/runner.tsx
+++ app/src/spotify_artist_traverse/runner.tsx
@@ -1,6 +1,6 @@
 const extension_id = "kmpbdkipjlpbckfnpbfbncddjaneeklc";
-const MAX_RUNNERS = 8;
-const SLEEP_MS = 100;
+const MAX_RUNNERS = 32;
+const SLEEP_MS = 10;
 
 export const cancelled: { cancelled?: boolean } = {};
 
