acf72222732d4a88a05dc0dd793c33dff2037ebd
dcep93 <dcep93@gmail.com>
2023-06-24 21:21:34 -0400

    x

--- node/run.ts
+++ node/run.ts
@@ -3,4 +3,11 @@ import * as fs from "fs";
 import { getToken } from "./getToken";
 import traverse from "./traverse";
 
-fs.readFile("./secrets.json").then(getToken).then(traverse);
+fs.readFile("./node/secrets.json", (err, secrets) =>
+  new Promise((resolve, reject) => {
+    if (err) return reject(err);
+    resolve(secrets);
+  })
+    .then(getToken)
+    .then(traverse)
+);
