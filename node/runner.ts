6ac978d6ce7ed422a6e049fe8a4c4245135b44d2
dcep93 <dcep93@gmail.com>
2023-06-25 00:47:23 -0400

    x

--- node/runner.ts
+++ node/runner.ts
@@ -1,7 +1,7 @@
 import { dumpVars } from "./dump";
 
-const MAX_RUNNERS = 8;
-const SLEEP_MS = 100;
+const MAX_RUNNERS = 16;
+const SLEEP_MS = 10;
 
 export const cancelled: { cancelled?: boolean } = {};
 
