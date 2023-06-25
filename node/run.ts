3ecb184826639fa61457e29b5fd8c5eb1978f852
dcep93 <dcep93@gmail.com>
2023-06-24 23:42:42 -0400

    x

--- node/run.ts
+++ node/run.ts
@@ -1,12 +1,13 @@
 // @ts-ignore
 import * as fs from "fs";
+// @ts-ignore
+import { MongoClient } from "mongodb";
 
 import { dumpVars } from "./dump";
 import { getToken } from "./getToken";
 import traverse from "./traverse";
 
 const MONGO_URL = "mongodb://localhost:27017/";
-const MongoClient = require("mongodb").MongoClient;
 
 fs.readFile("./node/secrets.json", (err, raw) =>
   new Promise((resolve, reject) => {
@@ -15,18 +16,15 @@ fs.readFile("./node/secrets.json", (err, raw) =>
   })
     .then(JSON.parse)
     .then(getToken)
-    .then(() => {
-      console.log("connecting");
-      MongoClient.connect(MONGO_URL, function (err, db) {
-        console.log("connected");
-        if (err) throw err;
-        console.log("no err");
-        dumpVars.collection = db.db("db").collection("collection");
-        console.log("traversing");
-        traverse().then(() => {
-          dumpVars.collection = null;
-          db.close();
-        });
+    .then(() => console.log("connecting"))
+    .then(() => MongoClient.connect(MONGO_URL))
+    .then((db) => {
+      console.log("connected");
+      dumpVars.collection = db.db("db").collection("collection");
+      console.log("traversing");
+      traverse().then(() => {
+        dumpVars.collection = null;
+        db.close();
       });
     })
 );
--- node/setup.sh
+++ node/setup.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y git npm
+sudo npm install -g ts-node mongodb
+sudo npm i --save-dev @types/node
+git clone https://github.com/dcep93/spotify_artist_traverse.git
+cd spotify_artist_traverse
+ts-node node/run.ts
