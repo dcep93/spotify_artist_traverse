e14b3134311ceda7b33b74156d15e38ebc857f45
dcep93 <dcep93@gmail.com>
2023-06-25 14:09:31 -0400

    x

--- node/ext.ts
+++ node/ext.ts
@@ -1,27 +1,21 @@
 import { log } from "./runner";
 
 export default function ext(data: any): Promise<any> {
-  return new Promise((resolve, reject) => {
-    try {
-      fetch(data.fetch.url, data.fetch.options)
-        .then((resp: any) =>
-          !resp.ok
-            ? resp
-                .text()
-                .then((text) => ({ text, data }))
-                .then(log)
-                .then(reject)
-            : (data.fetch.json ? resp.json() : resp.text()).then((msg) => ({
-                msg,
-                ok: true,
-              }))
-        )
-        .then(resolve);
-    } catch (e) {
-      Promise.resolve()
-        .then(() => ({ e }))
-        .then(log)
-        .then(reject);
-    }
-  });
+  return new Promise((resolve, reject) =>
+    fetch(data.fetch.url, data.fetch.options)
+      .then((resp: any) =>
+        !resp.ok
+          ? resp
+              .text()
+              .then((text) => ({ text, data }))
+              .then(log)
+              .then(reject)
+          : (data.fetch.json ? resp.json() : resp.text()).then((msg) => ({
+              msg,
+              ok: true,
+            }))
+      )
+      .then(resolve)
+      .catch(reject)
+  );
 }
