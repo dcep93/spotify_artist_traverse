9856ad2a0831ff5d9c3d7f0925ded06865d5a638
dcep93 <dcep93@gmail.com>
2023-06-24 23:43:13 -0400

    x

--- node/run.ts
+++ node/run.ts
@@ -7,7 +7,7 @@ import { dumpVars } from "./dump";
 import { getToken } from "./getToken";
 import traverse from "./traverse";
 
-const MONGO_URL = "mongodb://localhost:27017/";
+const MONGO_URL = "mongodb://127.0.0.1:27017/";
 
 fs.readFile("./node/secrets.json", (err, raw) =>
   new Promise((resolve, reject) => {
