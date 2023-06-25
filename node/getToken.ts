d1089a271024c4bc61b03a531f513798744f4bc3
dcep93 <dcep93@gmail.com>
2023-06-25 00:51:50 -0400

    x

--- node/getToken.ts
+++ node/getToken.ts
@@ -46,6 +46,7 @@ function refreshPartnerToken() {
         })
     )
     .then(() => {
+      delete tokens.timeout;
       log(tokens);
       tokens.timeout = setTimeout(
         refreshPartnerToken,
