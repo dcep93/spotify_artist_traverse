ddea4185ce2f8a82ef89cf0a998a141b7349c7db
dcep93 <dcep93@gmail.com>
2023-06-25 00:49:48 -0400

    x

--- node/ext.ts
+++ node/ext.ts
@@ -1,7 +1,14 @@
 export default function ext(data: any): Promise<any> {
   return new Promise((resolve, reject) =>
     fetch(data.fetch.url, data.fetch.options)
-      .then((resp: any) => (resp.ok ? resp : resp.text().then(reject)))
+      .then((resp: any) =>
+        resp.ok
+          ? resp
+          : resp
+              .text()
+              .then((text) => ({ text, data }))
+              .then(reject)
+      )
       .then((resp) => (data.fetch.json ? resp.json() : resp.text()))
       .then((msg) => ({ msg, ok: true }))
       .then(resolve)
