99812968ac6e845657b7b091a9cc73b2fed47ce3
dcep93 <dcep93@gmail.com>
2023-06-26 15:19:08 -0400

    x

---
+++ node/oneHitWonder.ts
@@ -0,0 +1,50 @@
+// @ts-ignore
+import * as fs from "fs";
+// @ts-ignore
+import { MongoClient } from "mongodb";
+
+import { TraverseState } from "./traverse";
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/";
+
+function oneHitWonder(collection, cache) {
+  Promise.resolve()
+    .then(() =>
+      Object.values(TraverseState).filter((s: any) => !isNaN(parseInt(s)))
+    )
+    .then((states) =>
+      states.map((state) => [
+        TraverseState[state],
+        Object.values(cache).filter((o: any) => o.state === state).length,
+      ])
+    )
+    .then(Object.fromEntries)
+    .then(console.log)
+    .then(() =>
+      collection.forEach(
+        (document) => cache[document.id] || console.log(document.profile)
+      )
+    );
+}
+
+new Promise((resolve) =>
+  fs.readFile("./cache.json", (err, raw) => {
+    if (err) return resolve(undefined);
+    Promise.resolve()
+      .then(() => raw.toString())
+      .then(JSON.parse)
+      .then(resolve);
+  })
+).then((cache) =>
+  Promise.resolve()
+    .then(() => MongoClient.connect(MONGO_URL))
+    .then((db) =>
+      Promise.resolve()
+        .then(() => db.db("db").collection("collection"))
+        .then((collection) => oneHitWonder(collection, cache))
+        .finally(() => {
+          console.log("closing");
+          return db.close();
+        })
+    )
+);
